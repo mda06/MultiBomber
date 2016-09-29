9c141a4c29d32ebf600fc5b5adad128064c39e9f
mda06 <michaelvm171@msn.com>
2016-09-29 15:15:30 +0200

    NameSystem shown now the name at the middle of the end of the player

--- MultiBomber-core/src/com/mda/bomb/ecs/systems/NameSystem.java
+++ MultiBomber-core/src/com/mda/bomb/ecs/systems/NameSystem.java
@@ -2,6 +2,7 @@ package com.mda.bomb.ecs.systems;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mda.bomb.ecs.components.CollisionComponent;
 import com.mda.bomb.ecs.components.NameComponent;
 import com.mda.bomb.ecs.components.PositionComponent;
 import com.mda.bomb.ecs.core.Entity;
@@ -10,6 +11,7 @@ import com.mda.bomb.ecs.core.RenderSystem;
 public class NameSystem extends RenderSystem {
 	private PositionComponent pc;
 	private NameComponent name;
+	private CollisionComponent cc;
 	private static BitmapFont font = new BitmapFont();
 	
 	@Override
@@ -19,9 +21,11 @@ public class NameSystem extends RenderSystem {
 	public void render(Entity e, SpriteBatch batch) {
 		pc = e.getAs(PositionComponent.class);
 		name = e.getAs(NameComponent.class);
+		cc = e.getAs(CollisionComponent.class);
 		if(pc == null || name == null) return;
 		
-		font.draw(batch, name.name, pc.x + 15, pc.y - 10);
+		float w = font.getBounds(name.name).width;
+		font.draw(batch, name.name, pc.x - w / 2, pc.y - cc.size.y / 2 + cc.offset.y);
 	}
 	
 	public void dispose() {
